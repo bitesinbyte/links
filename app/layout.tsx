4486d204dba9f18a1ece8419fa5042fe3474fab1
manishtiwari25 <manish.ertiwari@outlook.com>
2024-10-01 05:15:06 +0000

    fix: build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark", enableSystem: false }}>
 					<div className="relative flex flex-col h-screen">
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
